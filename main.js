8156a00876fbfe82db8b007562811527fc75a7c5
clyfe <cliff.kamau@student.moringaschool.com>
2023-07-07 20:47:46 +0300

    completed

--- main.js
+++ main.js
@@ -3,6 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartButtons = document.querySelectorAll('.like-glyph');
+
+// Function to simulate a server request
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNum = Math.random();
+      if (randomNum < 0.5) {
+        resolve('Request successful');
+      } else {
+        reject('Request failed');
+      }
+    }, 300);
+  });
+}
+
+// Function to handle the heart button click
+function handleHeartClick(event) {
+  const heartButton = event.target;
+  mimicServerCall()
+    .then((response) => {
+      // Server request successful
+      heartButton.classList.toggle('activated-heart');
+      if (heartButton.innerHTML === '♡') {
+        heartButton.innerHTML = '♥';
+      } else {
+        heartButton.innerHTML = '♡';
+      }
+    })
+    .catch((error) => {
+      // Server request failed
+      const errorModal = document.getElementById('modal');
+      const errorMessage = document.getElementById('modal-message');
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Add event listener to each heart button
+heartButtons.forEach((button) => {
+  button.addEventListener('click', handleHeartClick);
+});
+
 
 
 
